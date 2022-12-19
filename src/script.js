5beeb1800de2448d993f1294a50e98b1bc8af15a
Eliezer Chirino <emchch3001@gmail.com>
2022-12-19 17:49:06 -0300

    Add stars to the haunted house scene

--- src/script.js
+++ src/script.js
@@ -68,6 +68,45 @@ grassAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping;
 grassNormalTexture.wrapT = THREE.RepeatWrapping;
 grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
 
+const particleTexture5 = textureLoader.load("/textures/particles/5.png");
+const particleTexture8 = textureLoader.load("/textures/particles/8.png");
+const particleTexture9 = textureLoader.load("/textures/particles/9.png");
+
+/**
+ * Particles
+ */
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 20000;
+
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 40;
+  colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+const particlesMaterial = new THREE.PointsMaterial();
+particlesMaterial.size = 0.1;
+particlesMaterial.sizeAttenuation = true;
+particlesMaterial.color = new THREE.Color("white");
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture9;
+// particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+particles.position.y = 27;
+scene.add(particles);
+
 /**
  * House
  */
